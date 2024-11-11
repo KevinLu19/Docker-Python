2a1c3ceb4629ca73a4fe454d2971ac1e3914116e
Kevin Lu <kevinxz01@gmail.com>
2024-11-11 12:05:38 -0600

    bug[pytest]: fixed api call

--- unit-test/test_manga_chapter.py
+++ unit-test/test_manga_chapter.py
@@ -5,12 +5,28 @@ BASE_URL = "https://api.mangadex.org"
 def test_success():
     print("Starting Testing")
 
+
 # From src.manga_chapters.py file
 def test_get_manga_by_name():
     anime_title = "bleach"
-    request = requests.get(f"{BASE_URL}/manga", params={"title": anime_title})
-    
+    request = requests.get(f"{BASE_URL}/manga", params={"title": anime_title, "limit": 5})
+    response = request.json()
+
     if request:
-        print ([manga["id"] for manga in request.json()["data"]])
+        for manga in response["data"]:
+            attributes = manga["attributes"]["title"]
+
+            print (attributes["en"])
+
+        for status in response["data"]:
+            manga_status = status["attributes"]["status"]
+            print("Status:", manga_status)
+        
+        for chpt in response["data"]:
+            last_chpt =  chpt["attributes"]["lastChapter"]
+            print("Last Chapter:", last_chpt)
+        
+        
+        
         
     assert request.json()["data"] is not None
